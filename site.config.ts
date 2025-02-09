e680894535b07dbb17be1ac43607483a1273d967
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-02-09 15:35:20 -0300

    default icon off

--- site.config.ts
+++ site.config.ts
@@ -26,7 +26,7 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: 'https://www.carlosbronze.com.br/apple-touch-icon.png',
+  defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
