8389081c9ca912211674d86fad73e66e26a24e71
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-08-02 12:40:15 -0300

    boosteroid console message reference

--- lib/bootstrap-console.ts
+++ lib/bootstrap-console.ts
@@ -7,3 +7,6 @@ export function cbconsole() {
   console.log('%c✉️ Email - carlosbronze@gmail.com', t)
   console.groupEnd()
 }
+
+// Reference: boosteroid.com console message
+//  console.log(`%cWelcome to Boosteroid ❤️`, '\n    padding: 0.4rem 1.5rem; \n    font-family: "Lucida Console"; \n    font-size: 4em; line-height: 1.4em; \n    color: white;\n    border-radius: 10px 50px 50px 10px;\n    background-color: #4158D0; \n    background-image: linear-gradient(43deg, #150947 5%, #460697 50%, #951089 100%);\n')
