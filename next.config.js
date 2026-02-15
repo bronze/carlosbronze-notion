767a37840a6af190138da822a5ecef724a39e32d
Carlos Bronze <170220+bronze@users.noreply.github.com>
2026-02-15 18:46:47 -0300

    chore: remove unused imports and add linting scripts in package.json

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
-import path from 'node:path'
-import {fileURLToPath} from 'node:url'
-
-import bundleAnalyzer from '@next/bundle-analyzer'
-
-const withBundleAnalyzer=bundleAnalyzer({
-  enabled: process.env.ANALYZE==='true'
-})
-
 export default {
   staticPageGenerationTimeout: 300,
   images: {
@@ -24,19 +15,6 @@ export default {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
   },
-
-  // webpack: (config) => {
-  //   // Workaround for ensuring that `react` and `react-dom` resolve correctly
-  //   // when using a locally-linked version of `react-notion-x`.
-  //   // @see https://github.com/vercel/next.js/issues/50391
-  //   const dirname = path.dirname(fileURLToPath(import.meta.url))
-  //   config.resolve.alias.react = path.resolve(dirname, 'node_modules/react')
-  //   config.resolve.alias['react-dom'] = path.resolve(
-  //     dirname,
-  //     'node_modules/react-dom'
-  //   )
-  //   return config
-  // },
 
   // See https://react-tweet.vercel.app/next#troubleshooting
   transpilePackages: ['react-tweet'],
